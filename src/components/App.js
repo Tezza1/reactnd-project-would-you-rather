66270e2225398ac236279bd0ae2a9e924cf50557
Tezza1 <tshenker@gmail.com>
2019-09-17 22:32:53 +0900

    Feature Added Top of Page items
    Added Semantic UI
    Added Menu
    Added Header

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Menu from './Menu'
+import Header from './Header'
+
+function App() {
+  return (
+    <div>
+      <Menu />
+      <Header />
+    </div>
+  );
+}
+
+export default App
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Header.css'
+
+const Header = (props) => {
+  return (
+    <div className='ui container center aligned'>
+      <h1 className='header'>Would You Rather...</h1>
+    </div>
+  )
+}
+
+export default Header
