0860f08848fc1680ecdc5f3625646d1f252cc2c4
Tezza1 <tshenker@gmail.com>
2019-09-18 12:19:20 +0900

    Feture Added redux
    Added framework for redux

---
+++ src/reducers/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux';
+
+// combine reducers into root reducer (single object)
+// createStore function only accepts single reducer
+export default combineReducers({
+})
