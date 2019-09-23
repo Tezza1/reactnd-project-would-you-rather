cde6690201318331490debfcf2a21c9bac207a46
Tezza1 <tshenker@gmail.com>
2019-09-23 23:49:00 +0900

    feature added API calls
    Implemented basic API calls to test the data coming back

--- src/components/App.js
+++ src/components/App.js
@@ -3,8 +3,29 @@ import Menu from './Menu'
 import Header from './Header'
 import Login from './Login'
 // import LeaderBoard from './LeaderBoard'
+import * as API from '../utils/_DATA.js'
 
 class App extends Component {
+
+  componentDidMount() {
+    this.getQuestions()
+    this.getUsers()
+  }
+
+  getQuestions() {
+    API._getQuestions()
+      .then(questions => {
+        console.log('These are the questions:', questions)
+      })
+  }
+
+  getUsers() {
+    API._getUsers()
+      .then(users => {
+        console.log('These are the users:', users)
+      })
+  }
+
   render() {
     return (
       <div>
